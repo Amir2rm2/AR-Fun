ee8fe98965b3911a2b4cabbec9c7dac37c76d965
Amir Hossain <121275143+Amir2rm2@users.noreply.github.com>
2023-04-04 23:15:50 +0600

    Add files via upload

--- AR.py
+++ AR.py
@@ -1,7 +1,16 @@
 import os
 os.system("pkg install espeak")
 
+from os import system as osRUB
+from os import system as cmd
+os.system('clear')
+print("Welcome to the My World ")
  
+print('loading Modules ...\n')
+
+
+
+
 
 logo = (""" 
                   ▉▉▉▉
